04a2e6aee41dc3717f89af32895a209e883fa0d4
Jed Peek <jedpeek@yahoo.com>
2018-05-04 04:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,10 +80,15 @@ function endGame() {
 
 
 function moveDodger(e){
+  if()
     if (e.which === LEFT_ARROW) {
+      e.preventDefault();
+      e.stopPropagation();
       moveDodgerLeft();
     }
     else if (e.which === RIGHT_ARROW) {
+      e.preventDefault();
+      e.stopPropagation();
       moveDodgerRight();
     }
 }
